cc96cc0cda5835dff86acf50456e2a3a3c81d388
Carlos Doki <carlosdoki@Carloss-MacBook-Pro.local>
2017-03-16 18:54:07 -0300

    first commit

---
+++ firebaseSortData/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  firebaseSortData
+//
+//  Created by Carlos Doki on 16/03/17.
+//  Copyright © 2017 Carlos Doki. All rights reserved.
+//
+
+import Foundation
---
+++ firebaseSortData/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  firebaseSortData
+//
+//  Created by Carlos Doki on 16/03/17.
+//  Copyright © 2017 Carlos Doki. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
